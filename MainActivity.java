9fbb4a493b16c569722beec66dd25377cd528fe5
PlatinumDL <64115176+PlatinumDL@users.noreply.github.com>
2020-05-02 12:20:13 +0800

    Finished

--- MainActivity.java
+++ MainActivity.java
@@ -1,27 +1,36 @@
-package sg.edu.np.WhackAMole;
+package com.example.practical2;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
-
-    /* Hint
-        - The function setNewMole() uses the Random class to generate a random value ranged from 0 to 2.
-        - Feel free to modify the function to suit your program.
-    */
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "Whack-A-Mole";
+    private TextView Score;
+    private List<Button> ButtonList = new ArrayList<>();
+    int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button ButtonLeft = (Button) findViewById(R.id.ButtonLeft);
+        Button ButtonRight = (Button) findViewById(R.id.ButtonRight);
+        Button ButtonMiddle = (Button) findViewById(R.id.ButtonMiddle);
+        ButtonList.add(ButtonMiddle);
+        ButtonList.add(ButtonLeft);
+        ButtonList.add(ButtonRight);
+        Score = (TextView) findViewById(R.id.Score);
 
-        Log.v(TAG, "Finished Pre-Initialisation!");
+        ButtonLeft.setOnClickListener(this);
+        ButtonMiddle.setOnClickListener(this);
+        ButtonRight.setOnClickListener(this);
     }
-
     @Override
     protected void onStart(){
         super.onStart();
@@ -29,10 +38,45 @@ public class MainActivity extends AppCompatActivity {
         Log.v(TAG, "Starting GUI!");
     }
 
-
     public void setNewMole()
     {
+        for (Button i: ButtonList){
+            i.setText("O");
+        }
         Random ran = new Random();
         int randomLocation = ran.nextInt(3);
+        Button random = ButtonList.get(randomLocation);
+        random.setText("*");
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button temp = (Button) v;
+        switch (v.getId()){
+            case R.id.ButtonLeft:
+                Log.v(TAG,"Button Left Clicked!");
+                break;
+            case R.id.ButtonMiddle:
+                Log.v(TAG,"Button Middle Clicked!");
+                break;
+            case R.id.ButtonRight:
+                Log.v(TAG,"Button Right Clicked!");
+                break;
+        }
+        if (temp.getText() == "*"){
+            count = count + 1;
+            temp.setText("O");
+            String numberAsString = String.valueOf(count);
+            Score.setText(numberAsString);
+            Log.v(TAG,"Hit, score added!");
+            setNewMole();
+        }
+        else {
+            count = count - 1;
+            String numberAsString = String.valueOf(count);
+            Score.setText(numberAsString);
+            Log.v(TAG,"Missed, score deducted!");
+            setNewMole();
+        }
     }
 }
